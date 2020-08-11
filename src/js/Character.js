316df1cb5f1c5cb6351a96f3d9879b5795b22109
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2020-08-11 22:03:23 +0300

    add class

---
+++ src/js/Character.js
@@ -0,0 +1,10 @@
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.type = type;
+    this.health = health;
+    this.level = level;
+    this.attack = attack;
+    this.defence = defence;
+  }
+}
